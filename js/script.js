999561a45ab614b20b1684ab6d942ced9ed7649f
Konstantin <sayk.tiger09@gmail.com>
2022-06-17 15:40:23 +0300

    скрипт модульного окна

--- js/script.js
+++ js/script.js
@@ -90,4 +90,38 @@ document.addEventListener(`DOMContentLoaded`, () => {
     }
 
     setClock(`.timer`, deadLine);
+
+
+    //Модальное окно
+    const modalTrigger = document.querySelectorAll(`[data-modal]`),
+        modal = document.querySelector(`.modal`),
+        modalClose = document.querySelector(`.modal__close`);
+
+    modalTrigger.forEach(btn =>{
+        btn.addEventListener(`click`, (e) =>{
+            modal.classList.remove(`hide`);
+            modal.classList.add(`show`);
+
+            document.body.style.overflow = `hidden`;
+        });
+    });
+    function closeModal(){
+        modal.classList.add(`hide`);
+        modal.classList.remove(`show`);
+        document.body.style.overflow = ``;
+    }
+
+    modalClose.addEventListener(`click`, closeModal);
+
+    modal.addEventListener(`click`, (e) =>{
+        if (e.target === modal){
+            closeModal();
+        }
+    });
+
+    document.addEventListener(`keydown`, (e)=>{
+        if (e.code === `Escape` && modal.classList.contains(`show`)){
+            closeModal();
+        }
+    });
 });

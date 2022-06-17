b9ed22d76573c371c862bd1735314f62301a8441
Konstantin <sayk.tiger09@gmail.com>
2022-06-17 09:42:40 +0300

    добавлен скрипт таймера

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener(`DOMContentLoaded`, () => {
-    
+    //tabs
     const tabs = document.querySelectorAll(`.tabheader__item`),
         tabsContent = document.querySelectorAll(`.tabcontent`),
         tabsParent = document.querySelector(`.tabheader__items`);
@@ -34,4 +34,60 @@ document.addEventListener(`DOMContentLoaded`, () => {
     });
     hideTabContent();
     showTabContent();
+
+
+    //timer
+    const deadLine = `2022-08-18`;
+
+    function getZero (num){
+        if (num >= 0 && num < 10){
+            return `0${num}`;
+        } else if(num < 0){
+            return `00`;
+        }else {
+            return num;
+        }
+    }
+
+    function getTimeRamaining(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        const days = Math.floor(t/(1000*60*60*24)),
+            hours = Math.floor((t/(1000*60*60)) % 24),
+            minutes = Math.floor((t/(1000 * 60) % 60)),
+            seconds = Math.floor((t/1000) % 60);
+        return {
+            'total' : t,
+            'days' : days,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+            
+    }
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(`.timer`),
+            days = timer.querySelector(`#days`),
+            hours = timer.querySelector(`#hours`),
+            minutes = timer.querySelector(`#minutes`),
+            seconds = timer.querySelector(`#seconds`),
+            timerInterval = setInterval(updateClock, 1000);
+
+            updateClock();
+
+        function updateClock(){
+            const t = getTimeRamaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0){
+                clearInterval(timerInterval);
+            }
+        }
+    }
+
+    setClock(`.timer`, deadLine);
 });
